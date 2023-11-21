92e60d76dfecaa19d9eb834b555e6bec536c334e
Dmitriy Plyshevskiy <dim.nik84@gmail.com>
2023-11-21 18:14:20 +0300

    added comments

--- echo-bot.py
+++ echo-bot.py
@@ -4,18 +4,24 @@ from aiogram.filters import Command
 from bot_token import bot_token
 
 BOT_TOKEN = bot_token
+
+# Создаем объекты бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
+# Этот хэндлер будет срабатывать на команду /start
 async def process_start_command(message: Message):
   await message.answer(text='Привет!\nМеня зовут Повторяшка\nЕсли хочешь узнать что я могу напиши "/info"')
 
+# Этот хэндлер будет срабатывать на команду /info
 async def process_info_command(message: Message):
   await message.answer(text='Я отвечаю тебе твоими сообщениями. Вот такое я абышто!')
 
+# Этот хэндлер будет срабатывать на любые сообщения кроме /start и /info
 async def send_echo(message: Message):
   await message.reply(text=message.text)
 
+# Регистрируем хэндлеры
 dp.message.register(process_start_command, Command(commands=['start']))
 dp.message.register(process_info_command, Command(commands=['info']))
 dp.message.register(send_echo)
