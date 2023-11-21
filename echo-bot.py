cbe96e95c5fa7ee9455bcdd676a5666ec609d247
Dmitriy Plyshevskiy <dim.nik84@gmail.com>
2023-11-21 18:09:22 +0300

    removed decorators and added register for functions

--- echo-bot.py
+++ echo-bot.py
@@ -7,17 +7,18 @@ BOT_TOKEN = bot_token
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-@dp.message(Command(commands=['start']))
 async def process_start_command(message: Message):
   await message.answer(text='Привет!\nМеня зовут Повторяшка\nЕсли хочешь узнать что я могу напиши "/info"')
 
-@dp.message(Command(commands=['info']))
 async def process_info_command(message: Message):
   await message.answer(text='Я отвечаю тебе твоими сообщениями. Вот такое я абышто!')
 
-@dp.message()
 async def send_echo(message: Message):
   await message.reply(text=message.text)
+
+dp.message.register(process_start_command, Command(commands=['start']))
+dp.message.register(process_info_command, Command(commands=['info']))
+dp.message.register(send_echo)
 
 if __name__ == '__main__':
   dp.run_polling(bot)
